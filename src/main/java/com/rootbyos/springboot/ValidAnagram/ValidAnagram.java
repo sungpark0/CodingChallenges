555bc487d7ca011a88c78a809d8a77cbe0186752
sungmin park <sungpark.inbox@gmail.com>
2020-10-07 20:35:46 -0400

    Day3 ValidAnagram

--- src/main/java/com/rootbyos/springboot/ValidAnagram/ValidAnagram.java
+++ src/main/java/com/rootbyos/springboot/ValidAnagram/ValidAnagram.java
@@ -1,5 +1,8 @@
 package com.rootbyos.springboot.ValidAnagram;
 
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.Arrays;
 /**
  * 242. Valid Anagram
  * Given two strings s and t , write a function to determine if t is an anagram of s.
@@ -19,4 +22,24 @@ package com.rootbyos.springboot.ValidAnagram;
  * What if the inputs contain unicode characters? How would you adapt your solution to such case?
  */
 public class ValidAnagram {
+
+    public static void main(String[] args) {
+        String s = "poop";
+        String t = "oo2pp";
+
+        isAnagram(s,t);
+
+    }
+    public static boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        char[] firstString = s.toCharArray();
+        char[] secondString = t.toCharArray();
+
+        Arrays.sort(firstString);
+        Arrays.sort(secondString);
+
+       return Arrays.equals(firstString, secondString);
+    }
 }
--- src/test/java/com/rootbyos/springboot/ValidAnagram/ValidAnagramTest.java
+++ src/test/java/com/rootbyos/springboot/ValidAnagram/ValidAnagramTest.java
@@ -1,7 +1,29 @@
 package com.rootbyos.springboot.ValidAnagram;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidAnagramTest {
 
+    @ParameterizedTest
+    @ValueSource(strings = {"poop"})
+    void positiveTest(String str1) {
+        String str2 = "popo";
+        assertTrue(ValidAnagram.isAnagram(str1, str2));}
+
+    @ParameterizedTest
+    @ValueSource(strings = {"ham"})
+    void negativeTest(String stringOne){
+        String stringTwo = "sam";
+        assertFalse(ValidAnagram.isAnagram(stringOne, stringTwo));
+    }
+
+    @ParameterizedTest
+    @ValueSource (strings = "police")
+    void lengthNegativeTest(String stringOne){
+        String stringTwo = "polices";
+        assertFalse(ValidAnagram.isAnagram(stringOne, stringTwo));
+    }
 }
