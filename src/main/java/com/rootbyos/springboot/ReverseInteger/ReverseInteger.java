936a79bb0b8ba862fa664b9b007a32a1d515cadc
sungmin park <sungpark.inbox@gmail.com>
2020-10-09 00:59:50 -0400

    Day4 ReverseInteger

--- src/main/java/com/rootbyos/springboot/ReverseInteger/ReverseInteger.java
+++ src/main/java/com/rootbyos/springboot/ReverseInteger/ReverseInteger.java
@@ -21,5 +21,28 @@ package com.rootbyos.springboot.ReverseInteger;
  * the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem,
  * assume that your function returns 0 when the reversed integer overflows.
  */
+import java.lang.*;
+
 public class ReverseInteger {
+    public static void main(String[] args) {
+
+        System.out.println(reverse(12321));
+
+    }
+
+    public static int reverse(int num) {
+        long reversedNumber = 0;
+
+        while(num != 0) {
+            reversedNumber = (reversedNumber * 10) + (num % 10); //reveredNumber*10 moves digit and add num %10 to last
+            num = num / 10;
+        }
+
+        if(reversedNumber > Integer.MAX_VALUE || reversedNumber < Integer.MIN_VALUE) {
+            return 0; //checking for out of scope and return 0
+        }
+        else{
+            return (int) reversedNumber; //convert long to int
+        }
+    }
 }
--- src/test/java/com/rootbyos/springboot/ReverseInteger/ReverseIntegerTest.java
+++ src/test/java/com/rootbyos/springboot/ReverseInteger/ReverseIntegerTest.java
@@ -1,13 +1,24 @@
 package com.rootbyos.springboot.ReverseInteger;
 
 import com.rootbyos.springboot.StringReversal.StringReversal;
-import com.rootbyos.springboot.Vowels.Vowels;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class ReverseIntegerTest {
 
+    @ParameterizedTest
+    @ValueSource (ints = -12345)
+    void positiveTest (int testNum){
+        assertEquals(-54321, ReverseInteger.reverse(testNum));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = 214)
+    void negativeTest ( int testNum){
+        assertNotEquals(214, ReverseInteger.reverse(testNum)); //Not sure about this negative test.
+    }
 
 }
 
