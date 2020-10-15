9c2541774e62576185cdfe9ce5da2ea6b1693238
sungmin park <sungpark.inbox@gmail.com>
2020-10-15 13:41:15 -0400

    Day10 ValidAnagram

--- src/main/java/com/rootbyos/springboot/ValidAnagram/ValidAnagram.java
+++ src/main/java/com/rootbyos/springboot/ValidAnagram/ValidAnagram.java
@@ -1,23 +1,29 @@
 package com.rootbyos.springboot.ValidAnagram;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+
+import java.io.InputStream;
 import java.lang.reflect.Array;
 import java.util.*;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * 242. Valid Anagram
  * Given two strings s and t , write a function to determine if t is an anagram of s.
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: s = "anagram", t = "nagaram"
  * Output: true
  * Example 2:
- *
+ * <p>
  * Input: s = "rat", t = "car"
  * Output: false
  * Note:
  * You may assume the string contains only lowercase alphabets.
- *
+ * <p>
  * Follow up:
  * What if the inputs contain unicode characters? How would you adapt your solution to such case?
  */
@@ -25,21 +31,36 @@ public class ValidAnagram {
 
     public static void main(String[] args) {
         String s = "poop";
-        String t = "oo2pp";
+        String t = "oopp";
 
-        isAnagram(s,t);
+        isAnagram( s, t );
+        isAnaramStream( s, t );
 
     }
+
     public static boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
+        if (s.length() != t.length()) {
             return false;
         }
         char[] firstString = s.toCharArray();
         char[] secondString = t.toCharArray();
 
-        Arrays.sort(firstString);
-        Arrays.sort(secondString);
+        Arrays.sort( firstString );
+        Arrays.sort( secondString );
+        System.out.println();
 
-       return Arrays.equals(firstString, secondString);
+        return Arrays.equals( firstString, secondString );
     }
+
+    public static boolean isAnaramStream(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        return Arrays.stream( s.split( "" ) ).sorted().collect( Collectors.joining() ).equals( Arrays.stream( t.split( "" ) ).sorted().collect( Collectors.joining() ) );
+    }
+
+//    public static String sortedLetters(String str) {
+//        return Arrays.stream( str.split( "" ) ).sorted().collect( Collectors.joining() );
+//    }
+
 }
