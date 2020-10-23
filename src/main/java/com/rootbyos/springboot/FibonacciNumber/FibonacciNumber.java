1207e31167b5f6865588a0b6e85d3571f9fc651f
sungmin park <sungpark.inbox@gmail.com>
2020-10-23 01:39:57 -0400

    Day18 FibonacciNumber Final

--- src/main/java/com/rootbyos/springboot/FibonacciNumber/FibonacciNumber.java
+++ src/main/java/com/rootbyos/springboot/FibonacciNumber/FibonacciNumber.java
@@ -3,23 +3,31 @@ package com.rootbyos.springboot.FibonacciNumber;
 public class FibonacciNumber {
     public static void main(String[] args) {
 
-        System.out.println(fibRecursion(3));
-//        System.out.println(fib(3));
+        System.out.println( fibRecursion( 4 ) );
+        System.out.println( fib( 4 ) );
     }
 
-    public static int fibRecursion(int N){
-        if(N<=1){
+    public static int fibRecursion(int N) {
+        if (N <= 1) {
             return N;
         }
-        return fibRecursion(N-1) + fibRecursion(N-2);
+        return fibRecursion( N - 1 ) + fibRecursion( N - 2 );
     }
 
-//    public static int fib(int N){
-//        if(N<=1){
-//            return N;
-//        }
-//        while ( something){ add previous #'s }
-//
-//        // need to make a recurring loop to add whatever #s lower than int N
-//    }
+    public static int fib(int N) {
+        int[] f = new int[N + 1];
+
+        if (N == 0) {
+            return N;
+        }
+
+        f[1] = 1; //so we dont go below 0.
+
+        for (int i = 2; i <= N; i++) { //covering 0 and 1 with above methods so here we just need to sum 2 and above
+            f[i] = f[i - 1] + f[i - 2];
+        }
+
+        return f[N];
+    }
+
 }
