fa9883badf86df74e6352a4e741cdaf662206a74
sungmin park <sungpark.inbox@gmail.com>
2020-10-26 22:17:13 -0400

    Day22 LengthOfLastWord

--- src/main/java/com/rootbyos/springboot/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStock.java
+++ src/main/java/com/rootbyos/springboot/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStock.java
@@ -13,35 +13,45 @@ public class BestTimeToBuyAndSellStock {
     }
 
     public static int maxProfit(int[] prices) {
-        int sellMinusBuy;
-        int profit = 0;
+        int potentialProfit;
+        int maxProfit = 0;
 
         if (prices.length == 2 && prices[0] < prices[1]) {
             return prices[1] - prices[0];
         }
 
+
         for (int i = 0; i < prices.length - 1; i++) {
             for (int j = i + 1; j < prices.length; j++) {
-                sellMinusBuy = prices[j] - prices[i];
-                if (sellMinusBuy > profit) {
-                    profit = sellMinusBuy;
+                potentialProfit = prices[j] - prices[i];
+                if (potentialProfit > maxProfit) {
+                    maxProfit = potentialProfit;
                 }
             }
         }
-        return profit;
+        return maxProfit;
 
     }
 
     public static int maxProfitMath(int[] prices) {
-        if (prices.length < 2) return 0;
+        if (prices.length < 2)
+            return 0; // Because if less than 2 elements there cant be any transaction define by the problem.
 
-        int lowestVal = prices[0];
-        int maxProfit = prices[1] - prices[0];
-        for (int potentialVal : prices) {
+        //7 1 5 3 6 4
+        int lowestVal = prices[0]; // 7
+        int maxProfit = prices[1] - prices[0]; // 6-7 =-1
+        for (int potentialVal : prices) { // for-each loop from potentialVal to end of array prices.
+            // 7 1 5 3 6 4  to 7 1 5 3 6 4
+            // 7 1 5 3 6 4
+            //
+            //
+            // 7 1 5 3 6 4
             int potentialProfit = potentialVal - lowestVal;
 
             maxProfit = Math.max( maxProfit, potentialProfit );
+            // returns the larger number between maxProfit and potentialProfit
             lowestVal = Math.min( lowestVal, potentialVal );
+            // returns the smaller number between lowestVal and potentialVal
         }
 
         return maxProfit;
---
+++ src/main/java/com/rootbyos/springboot/LengthOfLastWord/LengthOfLastWord.java
@@ -0,0 +1,22 @@
+package com.rootbyos.springboot.LengthOfLastWord;
+
+import java.util.Arrays;
+
+public class LengthOfLastWord {
+
+    public static void main(String[] args) {
+
+        String test = "hell world";
+        System.out.println( lengthOfLastWord( test ) );
+    }
+
+    public static int lengthOfLastWord(String s) {
+        String[] splitSentence = s.split( " " ); // splitting the sentence into an array
+        if (splitSentence.length == 0) return 0; //if array is empty because string s is empty, return 0
+//        splitSentence.length==0 ? return 0 : (? );
+        String lastWord = splitSentence[splitSentence.length - 1]; //inputting last word into String lastWord
+
+
+        return lastWord.length();
+    }
+}
