8a53812c2c70eebb4a690f01cf58cdf7fe6ee63b
sungmin park <sungpark.inbox@gmail.com>
2020-10-29 17:38:17 -0400

    Day25 RansomNote Final

--- src/main/java/com/rootbyos/springboot/RansomNote/RansomNote.java
+++ src/main/java/com/rootbyos/springboot/RansomNote/RansomNote.java
@@ -6,36 +6,67 @@ public class RansomNote {
 
     public static void main(String[] args) {
 
-        String a = "aa";
-        String b = "aab";
-        canConstruct( a, b );
+        String ransom = "aa";
+        String magazine = "aab";
+        canConstruct( ransom, magazine );
     }
 
     public static boolean canConstruct(String ransomNote, String magazine) {
 
         char[] ransomArr = ransomNote.toCharArray();
-        Arrays.sort( ransomArr );
         char[] magArr = magazine.toCharArray();
-        Arrays.sort( magArr );
         char[] result = new char[ransomArr.length];
 
-        for(int i =0; i< ransomArr.length; i++){
-            for(int j=0; j<magArr.length; j++){
-                if(ransomArr[i]==magArr[j]){
+        for (int i = 0; i < ransomArr.length; i++) {
+            for (int j = 0; j < magArr.length; j++) {
+                if (ransomArr[i] == magArr[j]) {
                     result[i] = ransomArr[i];
-                    magArr[j]= 0;
+                    magArr[j] = 0;
+                    break;
                 }
             }
         }
-        System.out.println(Arrays.toString( result ));
-        System.out.println(Arrays.toString( ransomArr ));
-        if (Arrays.equals( result, ransomArr )) {
-            System.out.println("true");
-            return true;
-        } else {
-            System.out.println("false");
-            return false;
 
+
+        return Arrays.equals( result, ransomArr );
+    }
+
+    public static boolean canConstructReplaceMethod(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) return false;
+        int magLength = magazine.length();
+
+        for (char ele : ransomNote.toCharArray()) {
+            String str = String.valueOf( ele );
+            magazine = magazine.replaceFirst( str, "" );
         }
+
+        return magLength == magazine.length() + ransomNote.length();
+    }
+
+    public static boolean canConstructTwo(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) return false;
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char ele : magazine.toCharArray()) {
+            if (map.containsKey(ele)) {
+                map.put(ele, map.get(ele) + 1);
+            } else {
+                map.put(ele, 1);
+            }
+        }
+
+        for (char ele : ransomNote.toCharArray()) {
+            if (map.containsKey(ele)) {
+                if (map.get(ele) <= 0) {
+                    return false;
+                }
+                map.put(ele, map.get(ele) - 1);
+            } else {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
