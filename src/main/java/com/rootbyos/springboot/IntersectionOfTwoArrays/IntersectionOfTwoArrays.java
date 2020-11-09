d986c59608b010bc8f95ad82ac921434a7722e77
sungmin park <sungpark.inbox@gmail.com>
2020-11-08 20:48:55 -0500

    Day34 IntersectionOfTwoArrays I and II Final

--- src/main/java/com/rootbyos/springboot/IntersectionOfTwoArrays/IntersectionOfTwoArrays.java
+++ src/main/java/com/rootbyos/springboot/IntersectionOfTwoArrays/IntersectionOfTwoArrays.java
@@ -6,51 +6,51 @@ public class IntersectionOfTwoArrays {
 
     public static void main(String[] args) {
         int[] arr1 = {4, 9, 5};
-        int[] arr2 = {9,4,9,8,4};
-        System.out.println( Arrays.toString(intersection( arr1, arr2 )) );
-        System.out.println( Arrays.toString(intersectionI( arr1, arr2 )) );
-        System.out.println( Arrays.toString(intersectionII( arr1, arr2 )) );
-        System.out.println( Arrays.toString(intersectionIII( arr1, arr2 )) ); //fastest MAP
+        int[] arr2 = {9, 4, 9, 8, 4};
+        System.out.println( Arrays.toString( intersection( arr1, arr2 ) ) );
+        System.out.println( Arrays.toString( intersectionI( arr1, arr2 ) ) );
+        System.out.println( Arrays.toString( intersectionII( arr1, arr2 ) ) );
+        System.out.println( Arrays.toString( intersectionIII( arr1, arr2 ) ) ); //fastest MAP
     }
 
     public static int[] intersection(int[] nums1, int[] nums2) {
         List<Integer> firstArr = new ArrayList<>();
         List<Integer> result = new ArrayList<>();
 
-        for(int num : nums1){
-            firstArr.add(num);
+        for (int num : nums1) {
+            firstArr.add( num );
         }
 
-        for(int num : nums2 ){
-            if(firstArr.contains(num)){
-                result.add(num);
+        for (int num : nums2) {
+            if (firstArr.contains( num )) {
+                result.add( num );
             }
         }
 
-        return result.stream().distinct().mapToInt( i ->i ).toArray();
+        return result.stream().distinct().mapToInt( i -> i ).toArray();
     }
 
     public static int[] intersectionI(int[] nums1, int[] nums2) {
-        return Arrays.stream(nums1)
+        return Arrays.stream( nums1 )
                 .distinct()
-                .filter(num -> Arrays.stream(nums2).anyMatch(v -> v == num))
+                .filter( num -> Arrays.stream( nums2 ).anyMatch( v -> v == num ) )
                 .toArray();
     }
 
     public static int[] intersectionII(int[] nums1, int[] nums2) {
         List<Integer> result = new ArrayList<>();
-        int[] distinctArr = Arrays.stream(nums1).distinct().toArray();
+        int[] distinctArr = Arrays.stream( nums1 ).distinct().toArray();
 
         for (int num : distinctArr) {
-            if (Arrays.stream(nums2).anyMatch(v -> v == num)) {
-                result.add(num);
+            if (Arrays.stream( nums2 ).anyMatch( v -> v == num )) {
+                result.add( num );
             }
         }
 
         int[] arr = new int[result.size()];
 
         for (int i = 0; i < result.size(); i++) {
-            arr[i] = result.get(i);
+            arr[i] = result.get( i );
         }
         return arr;
 
@@ -63,17 +63,17 @@ public class IntersectionOfTwoArrays {
         List<Integer> result = new ArrayList<>();
 
         for (int num : nums1) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            map.put( num, map.getOrDefault( num, 0 ) + 1 );
         }
 
         for (int num : nums2) {
-            if (map.containsKey(num)) {
-                result.add(num);
-                map.remove(num);
+            if (map.containsKey( num )) {
+                result.add( num );
+                map.remove( num );
             }
         }
 
-        return result.stream().mapToInt(i -> i).toArray();
+        return result.stream().mapToInt( i -> i ).toArray();
     }
 
 }
--- src/main/java/com/rootbyos/springboot/IntersectionOfTwoArraysII/IntersectionOfTwoArraysII.java
+++ src/main/java/com/rootbyos/springboot/IntersectionOfTwoArraysII/IntersectionOfTwoArraysII.java
@@ -4,12 +4,62 @@ import java.util.*;
 
 public class IntersectionOfTwoArraysII {
     public static void main(String[] args) {
-        int[] nums1 = {1,2,2,1};
-        int[] nums2 = {2,2};
+        int[] nums1 = {4, 9, 5};
+        int[] nums2 = {9, 4, 9, 8, 4};
         System.out.println( Arrays.toString( intersect( nums1, nums2 ) ) );
+        System.out.println( Arrays.toString( intersectAttempTwo( nums1, nums2 ) ) );
+        System.out.println( Arrays.toString( intersectFinal( nums1, nums2 ) ) );
+        System.out.println( Arrays.toString( intersectII( nums1, nums2 ) ) );
     }
 
     public static int[] intersect(int[] nums1, int[] nums2) {
+        List<Integer> result = new ArrayList<>();
+        int[] temp = nums1;
+
+        if (nums1.length > nums2.length) {
+            nums1 = nums2;
+            nums2 = temp;
+        }//nums1 always will be smaller than nums 2
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    result.add( nums1[i] );
+                    break;
+                }
+            }
+        }
+        return result.stream().mapToInt( i -> i ).toArray();
+    }
+
+    public static int[] intersectFinal(int[] nums1, int[] nums2) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (int num : nums1) {
+            if (!map.containsKey( num )) {
+                map.put( num, 1 );
+
+            } else {
+                map.put( num, map.get( num ) + 1 );
+            }
+        }
+
+        for (int num : nums2) {
+            if (map.containsKey( num ) && map.get( num ) > 0) {
+                result.add( num );
+                map.put( num, map.get( num ) - 1 );
+            }
+        }
+//        int[] arr = new int[result.size()]; //USING THIS METHOD RATHER THAN THE BELOW MAKES THE RUN TIME FASTER
+//        for (int i = 0; i < result.size(); i++) {
+//            arr[i] = result.get( i );
+//        }
+//        return arr;
+        return result.stream().mapToInt( i -> i ).toArray();
+    }
+
+    public static int[] intersectAttempTwo(int[] nums1, int[] nums2) {
         List<Integer> smallerArr = new ArrayList<>();
         List<Integer> result = new ArrayList<>();
         int[] temp = nums1;
@@ -18,16 +68,47 @@ public class IntersectionOfTwoArraysII {
             nums1 = nums2;
             nums2 = temp;
         }
-        for(int num : nums2){
-            smallerArr.add(num);
+        for (int num : nums2) {
+            smallerArr.add( num );
         }
 
-        for(int num : nums1){
-            if(smallerArr.contains(num)){
-                result.add(num);
+        for (int num : nums1) {
+            if (smallerArr.contains( num )) {
+                result.add( num );
             }
         }
-        System.out.println(result);
+
         return result.stream().mapToInt( i -> i ).toArray();
+    }
+
+    public static int[] intersectII(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+
+        int[] shorterArr = nums1.length < nums2.length ? nums1 : nums2;
+        int[] longerArr = nums1.length < nums2.length ? nums2 : nums1;
+
+        for (int num : longerArr) {
+            map.put( num, map.getOrDefault( num, 0 ) + 1 );
+        }
+
+        for (int num : shorterArr) {
+            if (map.containsKey( num )) {
+                int val = map.get( num );
+                if (val == 0) {
+                    map.remove( num );
+                } else {
+                    result.add( num );
+                    map.put( num, val - 1 );
+                }
+            }
+        }
+
+        int[] arr = new int[result.size()];
+
+        for (int i = 0; i < result.size(); i++) {
+            arr[i] = result.get( i );
+        }
+        return arr;
     }
 }
