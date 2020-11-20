374da31d8b3347bbb4c7d1cb8007feff0b21f9c5
sungmin park <sungpark.inbox@gmail.com>
2020-11-20 01:30:57 -0500

    Day45 ValidParenthesesString Final

--- src/main/java/com/rootbyos/springboot/ValidParenthesisString/ValidParenthesesString.java
+++ src/main/java/com/rootbyos/springboot/ValidParenthesisString/ValidParenthesesString.java
@@ -1,46 +1,44 @@
 package com.rootbyos.springboot.ValidParenthesisString;
 
+import java.util.*;
+
 public class ValidParenthesesString {
     public static void main(String[] args) {
 
-        String test = ")(";
+        String test = "()";
 
         System.out.println(checkValidString(test));
 
     }
 
     public static boolean checkValidString(String s) {
+        List<Integer> left = new ArrayList<>();
+        List<Integer> asterisk = new ArrayList<>();
+        char[] chars = s.toCharArray();
 
-        int counteropen = 0;
-        int counterclose = 0;
-        int asteriskCounter = 0;
-        char[] ch = s.toCharArray();
-        for (int i = 0; i < ch.length; i++) {
-            int index = 0;
-            if (ch[i] == '(') {
-                counteropen++;
-                index = i;
-            } else if (ch[i] == ')' && counteropen>0) {
-                counterclose++;
-            } else if (ch[i] == '*') {
-                asteriskCounter++;
+        for (int i = 0; i < chars.length; i++) {
+            char ch = chars[i];
+            if (ch == '(') {
+                left.add(i);
+            } else if (ch == '*') {
+                asterisk.add(i);
+            } else {
+                if (left.isEmpty() && asterisk.isEmpty()) {
+                    return false;
+                } else if (left.isEmpty()) {
+                    asterisk.remove(asterisk.size() - 1);
+                } else {
+                    left.remove(left.size() - 1);
+                }
             }
         }
-        System.out.println(counteropen);
-        System.out.println(counterclose);
-        System.out.println(asteriskCounter);
-        if (counterclose == 1 && counteropen == 0) {
-            return false;
-        } else if ((counteropen + counterclose) % 2 != 0 && asteriskCounter % 2 != 0) {
-            return true;
-        } else if ((counteropen + counterclose) % 2 != 0 && asteriskCounter % 2 == 0) {
-            return true;
-        } else if ((counteropen + counterclose) % 2 == 0 && asteriskCounter % 2 != 0) {
-            return true;
-        } else if ((counteropen + counterclose) % 2 == 0 && asteriskCounter % 2 == 0) {
-            return true;
+        while (left.size() > 0 && asterisk.size() > 0) {
+            if (asterisk.get(asterisk.size() - 1) < left.get(left.size() - 1)) {
+                break;
+            }
+            asterisk.remove(asterisk.size() - 1);
+            left.remove(asterisk.size() - 1);
         }
-
-        return false;
+        return left.isEmpty();
     }
 }
