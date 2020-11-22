82f209e4d639261643b8e8a21f652d20147b6f6c
sungmin park <sungpark.inbox@gmail.com>
2020-11-21 22:32:58 -0500

    Day47 Island Perimeter Final

--- src/main/java/com/rootbyos/springboot/IslandPerimeter/IslandPerimeter.java
+++ src/main/java/com/rootbyos/springboot/IslandPerimeter/IslandPerimeter.java
@@ -2,23 +2,29 @@ package com.rootbyos.springboot.IslandPerimeter;
 
 public class IslandPerimeter {
     public static void main(String[] args) {
+
+        int[][] test = {{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}};
+        System.out.println(islandPerimeter(test));
     }
 
-    public static int islandPerimeter(int[][] grid){
-
-        int perimeter =0;
-
-        for(int i =0; i<grid.length; i++){ //add total
-            for(int j=0; j<grid[0].length; j++){
-                if(grid[i][j]==1){
-                    perimeter +=4;
+    public static int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+        for (int[] ints : grid) { //if it is an island count +4 for each side
+            for (int j = 0; j < grid[0].length; j++) {
+                if (ints[j] == 1) {
+                    perimeter += 4;
                 }
             }
         }
-        for (int i=0; i<grid.length; i++) { //Minus if nearby is an also island
-            for (int j=0; j<grid[0].length; j++) {
+        for (int i = 0; i < grid.length; i++) { //check if the immediate right and bottom is also an island. if they are -=2
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1) {
-                    
+                    if (i < grid.length - 1 && grid[i + 1][j] == 1) {
+                        perimeter -= 2;
+                    }
+                    if (j < grid[i].length - 1 && grid[i][j + 1] == 1) {
+                        perimeter -= 2;
+                    }
                 }
             }
         }
--- src/test/java/com/rootbyos/springboot/IslandPerimeter/IslandPerimeterTest.java
+++ src/test/java/com/rootbyos/springboot/IslandPerimeter/IslandPerimeterTest.java
@@ -1,7 +1,22 @@
 package com.rootbyos.springboot.IslandPerimeter;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class IslandPerimeterTest {
 
+    @Test
+    void positiveTest(){
+    int[][]test = {{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}};
+        assertEquals(16, IslandPerimeter.islandPerimeter(test));
+    }
+
+    @Test
+    void negativeTest(){
+        int[][]test = {{1, 1, 1, 1}, {0, 1, 1, 0}, {0, 1, 1, 1}, {1, 1, 0, 1}};
+        assertNotEquals(14, IslandPerimeter.islandPerimeter(test));
+    }
 }
