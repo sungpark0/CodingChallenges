d6d6128d180e6efbec88f61bf50f309915be3fdd
sungmin park <sungpark.inbox@gmail.com>
2020-12-02 01:00:44 -0500

    Day57 SpiralMatrix

---
+++ src/test/java/com/rootbyos/springboot/SpiralMatrix/SpiralMatrixTest.java
@@ -0,0 +1,24 @@
+package com.rootbyos.springboot.SpiralMatrix;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpiralMatrixTest {
+
+    @Test
+    void positiveTest() {
+        int[][] test = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        List<Integer> shouldBe = List.of(1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10);
+        assertEquals(shouldBe, SpiralMatrix.spiralOrder(test));
+    }
+
+    @Test
+    void negativeTest() {
+        int[][] test = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        List<Integer> shouldBe = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        assertNotEquals(shouldBe, SpiralMatrix.spiralOrder(test));
+    }
+}
