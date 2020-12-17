5620a4d5352a8bddb7dfb944a314a2119d75985f
sungmin park <sungpark.inbox@gmail.com>
2020-12-17 16:54:26 -0500

    Day69 MaximumRepeatingSubstring draft

---
+++ src/main/java/com/rootbyos/springboot/MaximumRepeatingSubstring/MaximumRepeatingSubstring.java
@@ -0,0 +1,19 @@
+package com.rootbyos.springboot.MaximumRepeatingSubstring;
+
+public class MaximumRepeatingSubstring {
+    public static void main(String[] args) {
+
+    }
+
+    public static int maxRepeating(String sequence, String word){
+        int counter =0;
+        char[] seqChar =sequence.toCharArray();
+        char[] wordChar = word.toCharArray();
+        int k= wordChar.length;
+
+        for(int i =0; i<seqChar.length; i+=k){
+        }
+
+        return counter;
+    }
+}
---
+++ src/test/java/com/rootbyos/springboot/MaximumRepeatingSubstring/MaximumRepeatingSubstringTest.java
@@ -0,0 +1,7 @@
+package com.rootbyos.springboot.MaximumRepeatingSubstring;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaximumRepeatingSubstringTest {
+
+}
