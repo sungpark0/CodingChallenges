8e51bb08c55c069e46fb82fd7770eaade915c960
sungmin park <sungpark.inbox@gmail.com>
2021-02-11 23:50:11 -0500

    Day120 Fibonacci Number & Climbing Stairs draft

---
+++ src/main/java/com/codingChallenge/ClimbingStairs/ClimbingStairs.java
@@ -0,0 +1,7 @@
+package com.codingChallenge.ClimbingStairs;
+
+public class ClimbingStairs {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/codingChallenge/ClimbingStairs/ClimbingStairsTest.java
@@ -0,0 +1,7 @@
+package com.codingChallenge.ClimbingStairs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClimbingStairsTest {
+
+}
