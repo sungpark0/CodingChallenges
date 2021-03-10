971c30e92889a3b16b6b5bdbf6dc8771e1ad2ae0
sungpark96 <72265464+sungpark96@users.noreply.github.com>
2021-03-09 21:07:35 -0500

    Day145 MinimumPathSum

--- src/main/java/com/codingChallenge/MinimumPathSum/MinimumPathSum.java
+++ src/main/java/com/codingChallenge/MinimumPathSum/MinimumPathSum.java
@@ -1,5 +1,7 @@
 package com.codingChallenge.MinimumPathSum;
 
+import java.util.Arrays;
+
 public class MinimumPathSum {
 
     public static int minPathSum(int[][] grid) {
@@ -23,11 +25,10 @@ public class MinimumPathSum {
             }
         }
 
-
         return dp[col - 1][row - 1];
     }
 
     public static void main(String[] args) {
-
+        System.out.println(minPathSum(new int[][]{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}}));
     }
 }
