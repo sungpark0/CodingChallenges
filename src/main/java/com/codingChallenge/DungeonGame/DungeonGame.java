8f4b3c57e72469283f01d891da62391e1be1a137
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-03-13 00:15:40 -0500

    Day148 DungeonGame Final

--- src/main/java/com/codingChallenge/DungeonGame/DungeonGame.java
+++ src/main/java/com/codingChallenge/DungeonGame/DungeonGame.java
@@ -1,6 +1,82 @@
 package com.codingChallenge.DungeonGame;
 
 public class DungeonGame {
+
+    public static int calculateMinimumHP(int[][] dungeon) {
+        int col = dungeon.length;
+        int row = dungeon[0].length;
+
+        int[][] dp = new int[col][row];
+        dp[col - 1][row - 1] = dungeon[col - 1][row - 1] < 0 ? Math.abs(dungeon[col - 1][row - 1]) + 1 : 1;
+
+        for (int i = col - 2; i >= 0; i--) {
+            int value = dp[i + 1][row - 1] - dungeon[i][row - 1];
+            dp[i][row - 1] = Math.max(value, 1);
+        }
+
+        for (int i = row - 2; i >= 0; i--) {
+            int value = dp[col - 1][i + 1] - dungeon[col - 1][i];
+            dp[col - 1][i] = Math.max(value, 1);
+        }
+
+        for (int i = col - 2; i >= 0; i--) {
+            for (int j = row - 2; j >= 0; j--) {
+                dp[i][j] = Math.min(Math.max(dp[i][j + 1] - dungeon[i][j], 1), Math.max(dp[i + 1][j] - dungeon[i][j], 1));
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    //         int col = dungeon.length;
+//         int row = dungeon[0].length;
+//         int[][] dp = new int[col][row];
+//         dp[0][0] = dungeon[0][0];
+//         int i = 1;
+//         int j = 1;
+//         while((i < col) && (j < row)){
+//             if(dungeon[i-1][j] > dungeon[i][j-1]){
+//                 dp[i-1][j] = dp[i-1][j-1] + dungeon[i-1][j];
+//                 j++;
+//             } else if(dungeon[i-1][j] < dungeon[i][j-1]) {
+//                 dp[i][j-1] = dp[i-1][j-1] + dungeon[i][j-1];
+//                 i++;
+//             }
+//         }
+//         if(i < col && j == row){
+//             while(i<col){
+//                 dp[i][j-1] = dp[i-1][j-1] + dungeon[i][j-1];
+//                 i++;
+//             }
+//         }
+//         if(i == col && j < row){
+//             while(j < row){
+//                 dp[i-1][j] = dp[i-1][j-1] + dungeon[i-1][j];
+//                 j++;
+//             }
+//         }
+//         System.out.println(Arrays.deepToString(dp));
+//         int negative = 0 ;
+//         int positive = 0;
+//         // List<Integer> negative = new ArrayList<>();
+//         // List<Integer> positive = new ArrayList<>();
+//         for(int x = 0; x<col; x++){
+//             for(int y = 0; y<row; y++){
+//                 if(dp[x][y] <= 0){
+//                     // negative.add(dungeon[x][y]);
+//                     negative += dungeon[x][y];
+//                 } else{
+//                     // positive.add(dungeon[x][y]);
+//                     positive += dungeon[x][y];
+//                 }
+//             }
+//         }
+//         System.out.println(negative);
+//         System.out.println(positive);
+//         if(positive > negative) return Math.abs(negative) + 1;
+//         // return !negative ? 1 : Math.abs(dp[col-1][row-1]) +1;
+//         // if(negative.isEmpty()) return 1;
+//         return Math.abs(dp[col-1][row-1]) + 1;
     public static void main(String[] args) {
 
     }
--- src/test/java/com/codingChallenge/DungeonGame/DungeonGameTest.java
+++ src/test/java/com/codingChallenge/DungeonGame/DungeonGameTest.java
@@ -1,7 +1,20 @@
 package com.codingChallenge.DungeonGame;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DungeonGameTest {
 
+    @Test
+    void positiveTest() {
+        int[][] test = new int[][]{{-2, -3, 3}, {-5, -10, -6}, {10, 30, -5}};
+        assertEquals(8, DungeonGame.calculateMinimumHP(test));
+    }
+
+    @Test
+    void negativeTest() {
+        int[][] test = new int[][]{{-10, 20, 30}, {-11, -17, -16}, {5, 1, -4}};
+        assertNotEquals(41, DungeonGame.calculateMinimumHP(test));
+    }
 }
