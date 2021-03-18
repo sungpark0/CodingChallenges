d041cc58d86ea691edb3eed5689bcab2f8597642
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-03-17 23:32:11 -0400

    Day152 PerfectSquare review

--- src/main/java/com/codingChallenge/PerfectSquares/PerfectSquares.java
+++ src/main/java/com/codingChallenge/PerfectSquares/PerfectSquares.java
@@ -5,17 +5,17 @@ public class PerfectSquares {
     public static int numSquares(int n) {
         int[] dp = new int[n + 1];
         dp[0] = 0;
+
         for (int i = 1; i <= n; i++) {
             dp[i] = i;
             for (int j = 1; j * j <= i; j++) {
                 dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
         }
-
         return dp[n];
     }
 
     public static void main(String[] args) {
-
+        System.out.println(numSquares(11));
     }
 }
