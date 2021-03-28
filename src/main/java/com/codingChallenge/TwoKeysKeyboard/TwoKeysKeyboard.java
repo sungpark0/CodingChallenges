391bc387ebcf6884a9190d4c9328633893b2523a
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-03-28 14:19:40 -0400

    Day162 TwoKeysKeyboard Review

--- src/main/java/com/codingChallenge/TwoKeysKeyboard/TwoKeysKeyboard.java
+++ src/main/java/com/codingChallenge/TwoKeysKeyboard/TwoKeysKeyboard.java
@@ -1,19 +1,20 @@
 package com.codingChallenge.TwoKeysKeyboard;
 
+import java.util.Arrays;
+
 public class TwoKeysKeyboard {
 
     public static int minSteps(int n) {
-        if(n == 1) return 0;
+        if (n == 1) return 0;
 
         int[] dp = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 0;
 
-        for(int i = 2; i <= n; i++){
+        for (int i = 2; i <= n; i++) {
             dp[i] = i;
-            for(int j = 2; j <= i; j++){
-                if(i % j == 0) {
-                    dp[i] = Math.min(dp[i], dp[j] + (i / j));
+            for (int j = i / 2; j > 1; j--) {
+                if (i % j == 0) {
+                    dp[i] = dp[j] + (i / j);
+                    break;
                 }
             }
         }
@@ -22,6 +23,6 @@ public class TwoKeysKeyboard {
     }
 
     public static void main(String[] args) {
-
+        System.out.println(minSteps(6));
     }
 }
