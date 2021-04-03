5adf407183b7151dfd47271b720bc245a9515dd0
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-04-02 21:31:47 -0400

    Day167 MinimumCostForTickets

---
+++ src/main/java/com/codingChallenge/MinimumCostForTickets/MinimumCostForTickets.java
@@ -0,0 +1,27 @@
+package com.codingChallenge.MinimumCostForTickets;
+
+public class MinimumCostForTickets {
+
+    public static int mincostTickets(int[] days, int[] costs) {
+        int n = days[days.length - 1];
+        int[] dp = new int[n + 1];
+
+        int j = 0;
+
+        for (int i = 1; i < dp.length; i++) {
+            if (days[j] == i) {
+                int daily = dp[i - 1] + costs[0];
+                int weekly = i > 7 ? dp[i - 7] + costs[1] : costs[1];
+                int monthly = i > 30 ? dp[i - 30] + costs[2] : costs[2];
+                dp[i] = Math.min(Math.min(daily, weekly), monthly);
+                j++;
+            } else dp[i] = dp[i - 1];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/codingChallenge/MinimumCostForTickets/MinimumCostForTicketsTest.java
@@ -0,0 +1,22 @@
+package com.codingChallenge.MinimumCostForTickets;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinimumCostForTicketsTest {
+
+    @Test
+    void positiveTest() {
+        int[] days = new int[]{1, 4, 6, 7, 8, 20};
+        int[] costs = new int[]{2, 7, 15};
+        assertEquals(11, MinimumCostForTickets.mincostTickets(days, costs));
+    }
+
+    @Test
+    void negativeTest() {
+        int[] days = new int[]{1, 4, 6, 10};
+        int[] costs = new int[]{7, 2, 15};
+        assertNotEquals(15, MinimumCostForTickets.mincostTickets(days, costs));
+    }
+}
