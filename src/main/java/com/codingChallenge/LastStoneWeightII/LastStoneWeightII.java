71bf2709f94a7891a5580c71854f3c2a3330b1b7
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-04-05 23:54:10 -0400

    Day170 LastStoneWeightII

--- src/main/java/com/codingChallenge/LastStoneWeightII/LastStoneWeightII.java
+++ src/main/java/com/codingChallenge/LastStoneWeightII/LastStoneWeightII.java
@@ -12,18 +12,18 @@ public class LastStoneWeightII {
 
         int[][] dp = new int[size + 1][total / 2 + 1];
 
-        for(int i = 1; i <= size; i++){
-            for(int j = 1; j <= total/2; j++){
-                if(stones[i - 1] <= j){
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= total / 2; j++) {
+                if (stones[i - 1] <= j) {
                     dp[i][j] = Math.max(dp[i - 1][j], stones[i - 1] + dp[i - 1][j - stones[i - 1]]);
                 } else dp[i][j] = dp[i - 1][j];
             }
         }
-
+        
         return total - 2 * dp[size][total / 2];
     }
 
     public static void main(String[] args) {
-
+        System.out.println(lastStoneWeightII(new int[]{2, 7, 4, 1, 8, 1}));
     }
 }
