ff1e7599524e971e37c9608086d940773809f9e8
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-04-11 23:38:46 -0400

    Day176 UniquePathsII

--- src/main/java/com/codingChallenge/UniquePathsII/UniquePathsII.java
+++ src/main/java/com/codingChallenge/UniquePathsII/UniquePathsII.java
@@ -8,19 +8,19 @@ public class UniquePathsII {
 
         int[][] dp = new int[col][row];
 
-        for (int i = 0; i < row; i++){
-            if(obstacleGrid[0][i] != 1) dp[0][i] = 1;
+        for (int i = 0; i < row; i++) {
+            if (obstacleGrid[0][i] != 1) dp[0][i] = 1;
             else break;
         }
 
-        for (int i = 0; i < col; i++){
-            if(obstacleGrid[i][0] != 1) dp[i][0] = 1;
+        for (int i = 0; i < col; i++) {
+            if (obstacleGrid[i][0] != 1) dp[i][0] = 1;
             else break;
         }
 
-        for (int i = 1; i < col; i++){
-            for (int j = 1; j < row; j++){
-                if(obstacleGrid[i][j] != 1) dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        for (int i = 1; i < col; i++) {
+            for (int j = 1; j < row; j++) {
+                if (obstacleGrid[i][j] != 1) dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
 
@@ -28,6 +28,6 @@ public class UniquePathsII {
     }
 
     public static void main(String[] args) {
-
+        System.out.println(uniquePathsWithObstacles(new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
     }
 }
