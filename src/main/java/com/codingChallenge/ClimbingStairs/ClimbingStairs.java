b9878c70fa2792ca4a54c098dd4e5f559b1c01dc
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-04-12 22:55:03 -0400

    Day177 Climbing Stairs

--- src/main/java/com/codingChallenge/ClimbingStairs/ClimbingStairs.java
+++ src/main/java/com/codingChallenge/ClimbingStairs/ClimbingStairs.java
@@ -3,19 +3,15 @@ package com.codingChallenge.ClimbingStairs;
 public class ClimbingStairs {
 
     public static int climbStairsTabulation(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
+        if(n <= 3) return n;
 
-        int[] arr = new int[n + 1];
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
 
-        arr[1] = 1;
-        arr[2] = 2;
+        for (int i = 3; i <= n; i++) dp[i] = dp[i - 1] + dp[i - 2];
 
-        for (int i = 3; i <= n; i++) {
-            arr[i] = arr[i - 1] + arr[i - 2];
-        }
-
-        return arr[n];
+        return dp[n];
     }
 
     public static int climbStairs(int n) {
