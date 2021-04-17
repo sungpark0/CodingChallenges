c9dd4fb43abd6477d187d84bb197fa3880fc7888
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-04-16 23:58:25 -0400

    Day181 PartitinEqualSubsetSum

--- src/main/java/com/codingChallenge/PartitionEqualSubsetSum/PartitionEqualSubsetSum.java
+++ src/main/java/com/codingChallenge/PartitionEqualSubsetSum/PartitionEqualSubsetSum.java
@@ -1,7 +1,37 @@
 package com.codingChallenge.PartitionEqualSubsetSum;
 
 public class PartitionEqualSubsetSum {
-    public static void main(String[] args) {
 
+    public static boolean canPartition(int[] nums) {
+
+        int total = 0;
+
+        for (int num : nums) total += num;
+
+        //If total is odd number its automatically false becase decimal number would be double instead of int
+        if (total % 2 != 0) return false;
+
+        int half = total / 2;
+        boolean[][] dp = new boolean[nums.length][half + 1];
+
+        for (int i = 0; i < nums.length; i++) dp[i][0] = true;
+
+        for (int i = 1; i <= half; i++) dp[0][i] = (nums[0] == i);
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 1; j <= half; j++) {
+                if (dp[i - 1][j]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else if (j >= nums[i]) {
+                    dp[i][j] = dp[i - 1][j - nums[i]];
+                }
+            }
+        }
+
+        return dp[nums.length - 1][half];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(canPartition(new int[]{1, 5, 11, 5}));
     }
 }
--- src/test/java/com/codingChallenge/PartitionEqualSubsetSum/PartitionEqualSubsetSumTest.java
+++ src/test/java/com/codingChallenge/PartitionEqualSubsetSum/PartitionEqualSubsetSumTest.java
@@ -1,7 +1,18 @@
 package com.codingChallenge.PartitionEqualSubsetSum;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PartitionEqualSubsetSumTest {
 
+    @Test
+    void positiveTest() {
+        assertTrue(PartitionEqualSubsetSum.canPartition(new int[]{1, 5, 11, 5}));
+    }
+
+    @Test
+    void negativeTest() {
+        assertFalse(PartitionEqualSubsetSum.canPartition(new int[]{1, 2, 3, 5}));
+    }
 }
