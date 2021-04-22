f9fe6bc0c296814cbe7904de1acd2247bb566d82
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-04-22 00:14:43 -0400

    Day185 NumberOfLongestIncreasingSubsequence Final

--- src/main/java/com/codingChallenge/NumberOfLongestIncreasingSubsequence/NumberOfLongestIncreasingSubsequence.java
+++ src/main/java/com/codingChallenge/NumberOfLongestIncreasingSubsequence/NumberOfLongestIncreasingSubsequence.java
@@ -1,31 +1,37 @@
 package com.codingChallenge.NumberOfLongestIncreasingSubsequence;
 
+import java.util.Arrays;
+
 public class NumberOfLongestIncreasingSubsequence {
 
     public static int findNumberOfLIS(int[] nums) {
         int[] lengthDP = new int[nums.length];
-        int[] countDP = new int[nums.length];
+        int[] counts = new int[nums.length];
         int maxLength = 1;
+        Arrays.fill(lengthDP, 1);
+        Arrays.fill(counts, 1);
 
-        for (int i = 0; i < nums.length; i++) {
-            lengthDP[i] = 1;
-            countDP[i] = 1;
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    if (lengthDP[i] < lengthDP[j] + 1) {
-                        lengthDP[i] = lengthDP[j] + 1;
-                        countDP[i] = countDP[j];
-                        maxLength = Math.max(maxLength, lengthDP[i]);
-                    } else if (lengthDP[i] == lengthDP[j] + 1) {
-                        countDP[i] += countDP[j];
+        for (int i = 0; i < nums.length - 1; i++) {
+            int length = lengthDP[i] + 1;
+            int count = counts[i];
+            boolean bool = false;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] > nums[i]) {
+                    if (length == lengthDP[j]) {
+                        counts[j] += count;
+                    } else if (length > lengthDP[j]) {
+                        bool = true;
+                        lengthDP[j] = length;
+                        counts[j] = count;
                     }
                 }
             }
+            maxLength = (bool && (length > maxLength)) ? length : maxLength;
         }
 
         int result = 0;
-        for (int i = 0; i < lengthDP.length; i++) {
-            if (lengthDP[i] == maxLength) result += countDP[i];
+        for (int i = 0; i < nums.length; i++) {
+            if (maxLength == lengthDP[i]) result += counts[i];
         }
 
         return result;
