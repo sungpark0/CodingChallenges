88bf7c1f2b6db203c7aa9600d2e7de10c4833d96
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-04-28 16:53:24 -0400

    Day192 LongestPalindromeSubstring

--- src/main/java/com/codingChallenge/LongestPalindromeSubstring/LongestPalindromeSubstring.java
+++ src/main/java/com/codingChallenge/LongestPalindromeSubstring/LongestPalindromeSubstring.java
@@ -7,11 +7,11 @@ public class LongestPalindromeSubstring {
         int start = 0;
         int end = 0;
 
-        for(int i = 0; i < s.length(); i++){
-            for(int j = i; j >= 0; j--){
-                if(i == j) dp[i][j] = true;
-                else if(s.charAt(i) == s.charAt(j)){
-                    if(i - j == 1 || dp[i - 1][j + 1]) dp[i][j] = true;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j >= 0; j--) {
+                if (i == j) dp[i][j] = true;
+                else if (s.charAt(i) == s.charAt(j)) {
+                    if (i - j == 1 || dp[i - 1][j + 1]) dp[i][j] = true;
                 }
 
                 if (dp[i][j] && (i - j > end - start)) {
@@ -24,32 +24,31 @@ public class LongestPalindromeSubstring {
         return s.substring(start, end + 1);
     }
 
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("toyota"));
+    }
+
     public String longestPalindromeII(String s) {
-        if(s.length() <= 1) return s;
+        if (s.length() <= 1) return s;
 
         String result = "";
-        for(int i = 0; i < s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
             String odd = pivotMiddle(s, i, i);
             String even = pivotMiddle(s, i, i + 1);
-            if(odd.length() > result.length()) result = odd;
-            if(even.length() > result.length()) result = even;
+            if (odd.length() > result.length()) result = odd;
+            if (even.length() > result.length()) result = even;
         }
 
         return result;
     }
 
-    public String pivotMiddle(String s, int left, int right){
-
-        String temp = s;
-
-        while(left >= 0 && right <s.length() && s.charAt(left) == s.charAt(right)){
+    public String pivotMiddle(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
 
-        return temp.substring(left + 1, right);
+        return s.substring(left + 1, right);
     }
-    public static void main(String[] args) {
 
-    }
 }
