6575ab9a0bf2b8f50c7b26330d9fa8d328e2b674
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-05-01 00:58:39 -0400

    Day194 BestTimeToBuyAndSellStockWithTransactionFee Final

--- src/main/java/com/codingChallenge/BestTimeToBuyAndSellStockWithTranscationFee/BestTimeToBuyAndSellStockWithTransactionFee.java
+++ src/main/java/com/codingChallenge/BestTimeToBuyAndSellStockWithTranscationFee/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -18,11 +18,11 @@ public class BestTimeToBuyAndSellStockWithTransactionFee {
             }
             profit = Math.max(profit, dp[i]);
         }
-
+        
         return profit;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2));
     }
 }
