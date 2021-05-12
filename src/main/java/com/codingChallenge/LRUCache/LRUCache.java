c3bd8f5699ea769fd4aebc931666bcf0dd76a687
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-05-12 00:22:27 -0400

    Day205 LRUCache final

--- src/main/java/com/codingChallenge/LRUCache/LRUCache.java
+++ src/main/java/com/codingChallenge/LRUCache/LRUCache.java
@@ -1,8 +1,36 @@
 package com.codingChallenge.LRUCache;
 
+import java.util.*;
+
 public class LRUCache {
 
+    Map<Integer, Integer> map = new HashMap<>();
+    Queue<Integer> queue = new LinkedList<>();
+    int cap;
+
+    public LRUCache(int capacity) {
+        this.cap = capacity;
+    }
+
+    public LRUCache() {}
+
+    public int get(int key) {
+        if(!map.containsKey(key)) return -1;
+
+        queue.remove(key);
+        queue.offer(key);
+        return map.get(key);
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) queue.remove(key);
+        else if (queue.size() == cap) map.remove(queue.poll());
+
+        queue.offer(key);
+        map.put(key, value);
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
--- src/test/java/com/codingChallenge/LRUCache/LRUCacheTest.java
+++ src/test/java/com/codingChallenge/LRUCache/LRUCacheTest.java
@@ -1,7 +1,24 @@
 package com.codingChallenge.LRUCache;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LRUCacheTest {
+
+    LRUCache test = new LRUCache();
+
+    @Test
+    void positiveTest() {
+        assertEquals(-1, test.get(4));
+    }
+
+    @Test
+    void negativeTest() {
+        test.put(1, 4);
+        assertNotEquals(-1, test.get(1));
+        //Testing put works
+        assertEquals(4, test.get(1));
+    }
 
 }
