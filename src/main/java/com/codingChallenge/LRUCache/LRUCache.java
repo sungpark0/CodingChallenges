aacc31d36826bd66df4cd301f03ae459e90ecdfc
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-05-12 00:02:16 -0400

    Day205 LRUCache temp

---
+++ src/main/java/com/codingChallenge/LRUCache/LRUCache.java
@@ -0,0 +1,8 @@
+package com.codingChallenge.LRUCache;
+
+public class LRUCache {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/test/java/com/codingChallenge/LRUCache/LRUCacheTest.java
@@ -0,0 +1,7 @@
+package com.codingChallenge.LRUCache;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LRUCacheTest {
+
+}
