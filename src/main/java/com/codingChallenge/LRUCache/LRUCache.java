b5294c81a9d37d872778b9fa53e24edf91f60548
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-05-13 00:08:50 -0400

    Day206 LRU changed to LinkedList

--- src/main/java/com/codingChallenge/LRUCache/LRUCache.java
+++ src/main/java/com/codingChallenge/LRUCache/LRUCache.java
@@ -4,30 +4,62 @@ import java.util.*;
 
 public class LRUCache {
 
-    Map<Integer, Integer> map = new HashMap<>();
-    Queue<Integer> queue = new LinkedList<>();
+    class Node {
+        int val;
+        Node prev;
+        Node next;
+        int key;
+
+        Node(int key, int val){
+            this.val = val;
+            this.key = key;
+        }
+    }
+
+    Node head = new Node(0, 0);
+    Node tail = new Node(0, 0);
+    Map<Integer, Node> map = new HashMap<>();
     int cap;
 
     public LRUCache(int capacity) {
         this.cap = capacity;
+        map = new HashMap(capacity);
+
+        head.next = tail;
+        tail.prev = head;
     }
 
-    public LRUCache() {}
-
     public int get(int key) {
-        if(!map.containsKey(key)) return -1;
+        Node node = map.get(key);
+        if(node == null) return -1;
 
-        queue.remove(key);
-        queue.offer(key);
-        return map.get(key);
+        delete(node);
+        insert(node);
+        return node.val;
     }
 
     public void put(int key, int value) {
-        if (map.containsKey(key)) queue.remove(key);
-        else if (queue.size() == cap) map.remove(queue.poll());
+        if (map.containsKey(key)) {
+            delete(map.get(key));
+        } else if (map.size() == cap){
+            delete(tail.prev);
+        }
 
-        queue.offer(key);
-        map.put(key, value);
+        insert(new Node(key, value));
+    }
+
+    public void insert(Node node){
+        map.put(node.key, node);
+        node.next=head.next;
+        head.next.prev=node;
+        node.prev=head;
+        head.next=node;
+    }
+
+    public void delete(Node node){
+        map.remove(node.key);
+        node.prev.next=node.next;
+        node.next.prev=node.prev;
     }
 
     public static void main(String[] args) {
