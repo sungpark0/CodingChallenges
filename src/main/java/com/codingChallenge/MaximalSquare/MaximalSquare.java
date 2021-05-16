95352c0a46fc6ab80e1df02928f4083d21f57a99
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-05-15 23:57:19 -0400

    Day209 MaximalSquare

--- src/main/java/com/codingChallenge/MaximalSquare/MaximalSquare.java
+++ src/main/java/com/codingChallenge/MaximalSquare/MaximalSquare.java
@@ -22,6 +22,24 @@ public class MaximalSquare {
         return length * length;
     }
 
+    public int maximalSquareII(char[][] matrix) {
+        int length = 0;
+        int[][] dp = new int[matrix.length][matrix[0].length];
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                dp[i][j] = matrix[i][j] - '0';
+                if(i == 0 || j == 0) length = Math.max(length, dp[i][j]);
+                else if(matrix[i][j] == '1'){
+                    dp[i][j] = Math.min(dp[i][j - 1], Math.min(dp[i - 1][j], dp[i - 1][j - 1])) + 1;
+                    length = Math.max(length, dp[i][j]);
+                }
+            }
+        }
+
+        return length * length;
+    }
+
     public static void main(String[] args) {
         System.out.println(maximalSquare(new char[][]{
                 {'1', '0', '1', '0', '0'},
