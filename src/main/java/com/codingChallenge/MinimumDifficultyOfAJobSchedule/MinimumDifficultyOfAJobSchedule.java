d3c8eea8875ebc72c7db8c0cbb8027b034cfc705
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-05-27 01:18:06 -0400

    Day219 MinimumDifficultyOfAJobSchedule final

--- src/main/java/com/codingChallenge/MinimumDifficultyOfAJobSchedule/MinimumDifficultyOfAJobSchedule.java
+++ src/main/java/com/codingChallenge/MinimumDifficultyOfAJobSchedule/MinimumDifficultyOfAJobSchedule.java
@@ -1,7 +1,32 @@
 package com.codingChallenge.MinimumDifficultyOfAJobSchedule;
 
 public class MinimumDifficultyOfAJobSchedule {
+
+    public static int minDifficulty(int[] jobDifficulty, int d) {
+        if(jobDifficulty.length < d) return -1;
+
+        int[][] dp = new int[d][jobDifficulty.length];
+        dp[0][0] = jobDifficulty[0];
+
+        for(int i = 1;  i < jobDifficulty.length; i++) {
+            dp[0][i] = Math.max(dp[0][i - 1], jobDifficulty[i]);
+        }
+
+        for (int i = 1 ; i < d; i++) {
+            for (int j = i; j < jobDifficulty.length; j++) {
+                int temp = jobDifficulty[j];
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = j; k >= i; k--) {
+                    temp = Math.max(temp, jobDifficulty[k]);
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][k - 1] + temp);
+                }
+            }
+        }
+
+        return dp[d - 1][jobDifficulty.length - 1];
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
--- src/test/java/com/codingChallenge/MinimumDifficultyOfAJobSchedule/MinimumDifficultyOfAJobScheduleTest.java
+++ src/test/java/com/codingChallenge/MinimumDifficultyOfAJobSchedule/MinimumDifficultyOfAJobScheduleTest.java
@@ -1,7 +1,19 @@
 package com.codingChallenge.MinimumDifficultyOfAJobSchedule;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MinimumDifficultyOfAJobScheduleTest {
+
+    @Test
+    void positiveTest() {
+        assertEquals(15, MinimumDifficultyOfAJobSchedule.minDifficulty(new int[]{7, 1, 7, 1, 7, 1}, 3));
+    }
+
+    @Test
+    void negativeTest(){
+        assertNotEquals(-1, MinimumDifficultyOfAJobSchedule.minDifficulty(new int[]{1,1,1}, 3));
+    }
 
 }
