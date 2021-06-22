7bd7d16aaa0be931261f644772cef6b68df62bf5
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-06-22 13:38:08 -0400

    Day246 HeightChecker

--- src/main/java/com/codingChallenge/HeightChecker/HeightChecker.java
+++ src/main/java/com/codingChallenge/HeightChecker/HeightChecker.java
@@ -9,35 +9,23 @@ public class HeightChecker {
 
         int[] test = {5, 3, 3, 4, 5};
 //        System.out.println( heightChecker( test ) );
-        System.out.println( heightCheckerTwo( test ) );
+        System.out.println(heightCheckerTwo(test));
     }
 
     public static int heightCheckerTwo(int[] heights) {
-
         int[] temp = heights.clone();
-        Arrays.sort( heights );
+        Arrays.sort(heights);
 
         int counter = 0;
-//        int dump = 0;
-//        int j=0;
-//        for (int num : heights) { // using for-each loop as practice
-//            if (num != temp[j]) {
-//                counter++;
-//            }
-//            j++;
-//        }
         for (int i = 0; i < heights.length; i++) {
-//            dump = temp[i] != heights[i] ? counter++ : dump++; //Not a practical use because we only care for the value of counter
-            if (temp[i] != heights[i]) {
-                counter++;
-            }
+            if (temp[i] != heights[i]) counter++;
+
         }
 
         return counter;
-
     }
 
-//    public static int heightChecker(int[] heights) {
+    //    public static int heightChecker(int[] heights) {
 //        int[] copy = heights.clone();
 //        int counter = 0;
 //        Arrays.sort( heights );
@@ -54,10 +42,10 @@ public class HeightChecker {
 //            copy[i] = heights[i];
 //        }
     public static int heightChecker(int[] heights) {
-        int[] copy = Arrays.stream( heights ).sorted().toArray();
+        int[] copy = Arrays.stream(heights).sorted().toArray();
 
-        return (int) IntStream.range( 0, heights.length )
-                .filter( i -> copy[i] != heights[i] )
+        return (int) IntStream.range(0, heights.length)
+                .filter(i -> copy[i] != heights[i])
                 .count();
     }
 }
