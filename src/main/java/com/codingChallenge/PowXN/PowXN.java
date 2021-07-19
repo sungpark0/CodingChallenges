e387eb056295765d869e195e421ffc7861236e67
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-07-18 23:49:47 -0400

    Day272 PowXN, SpiralMatrix, IntersectingLinkedList

---
+++ src/main/java/com/codingChallenge/PowXN/PowXN.java
@@ -0,0 +1,7 @@
+package com.codingChallenge.PowXN;
+
+public class PowXN {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/codingChallenge/PowXN/PowXNTest.java
@@ -0,0 +1,7 @@
+package com.codingChallenge.PowXN;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PowXNTest {
+
+}
