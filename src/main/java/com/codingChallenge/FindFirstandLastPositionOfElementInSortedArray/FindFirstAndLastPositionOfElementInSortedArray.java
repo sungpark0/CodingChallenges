eafeff6e70927be2c5f9c4c82731e2f9c9f1784a
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-09-03 22:43:19 -0400

    Day319 FindTheFirstAndLastPositionOfElementInSortedArray

--- src/main/java/com/codingChallenge/FindFirstandLastPositionOfElementInSortedArray/FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/com/codingChallenge/FindFirstandLastPositionOfElementInSortedArray/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -10,8 +10,8 @@ public class FindFirstAndLastPositionOfElementInSortedArray {
     public static void main(String[] args) {
         int[] nums = {2, 2};
         int target = 2;
-        System.out.println(Arrays.toString(findTheHighAndLowIndex(List.of(1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6), 2))); // low = 15, high = 17
-        System.out.println(Arrays.toString(findTheHighAndLowIndex(List.of(1), 1))); // low = 15, high = 17
+//        System.out.println(Arrays.toString(findTheHighAndLowIndex(List.of(1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6), 2))); // low = 15, high = 17
+//        System.out.println(Arrays.toString(findTheHighAndLowIndex(List.of(1), 1))); // low = 15, high = 17
     }
 
 //    public static int[] searchRange(int[] nums, int target) {
@@ -41,36 +41,68 @@ public class FindFirstAndLastPositionOfElementInSortedArray {
 //            return new int[]{-1, -1};
 //        }
 //        return new int[]{firstIndex, lastIndex - 1};
-    public static int[] findTheHighAndLowIndex(List<Integer> arr, int key) {
-        return new int[]{
-                findLowIndex(arr, 0, arr.size() - 1, key),
-                findHighIndex(arr, 0, arr.size() - 1, key)};
+//    public static int[] findTheHighAndLowIndex(List<Integer> arr, int key) {
+//        return new int[]{
+//                findLowIndex(arr, 0, arr.size() - 1, key),
+//                findHighIndex(arr, 0, arr.size() - 1, key)};
+//
+//    }
+//
+//    static int findLowIndex(List<Integer> arr, int low, int high, int key) {
+//        if (high < low) return -1;
+//        if (high == low) return low;
+//        int mid = (low + high) / 2;
+//
+//
+//        if (arr.get(mid) == key && arr.get(mid - 1) != key) return mid;
+//
+//        if (arr.get(mid) < key) return findLowIndex(arr, mid, high, key);
+//
+//        return findLowIndex(arr, low, mid, key);
+//    }
+//
+//    public static int findHighIndex(List<Integer> arr, int low, int high, int key) {
+//        if (high < low) return -1;
+//        if (high == low) return high;
+//        int mid = (low + high) / 2;
+//
+//        if (arr.get(mid) == key && arr.get(mid + 1) != key) return mid;
+//
+//        if (arr.get(mid) > key) return findHighIndex(arr, low, mid, key);
+//
+//        return findHighIndex(arr, mid, high, key);
+//    }
 
-    }
+    public static int[] searchRange(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length;
+        int mid;
 
-    static int findLowIndex(List<Integer> arr, int low, int high, int key) {
-        if (high < low) return -1;
-        if (high == low) return low;
-        int mid = (low + high) / 2;
+        while (start < end) {
+            mid = (start + end) / 2;
 
+            if (nums[mid] >= target) end = mid;
+            else start = mid + 1;
+        }
 
-        if (arr.get(mid) == key && arr.get(mid - 1) != key) return mid;
+        int firstIndex = start;
+        start = 0;
+        end = nums.length;
 
-        if (arr.get(mid) < key) return findLowIndex(arr, mid, high, key);
+        while (start < end) {
+            mid = (start + end) / 2;
 
-        return findLowIndex(arr, low, mid, key);
-    }
+            if (nums[mid] > target) end = mid;
+            else start = mid + 1;
+        }
 
-    public static int findHighIndex(List<Integer> arr, int low, int high, int key) {
-        if (high < low) return -1;
-        if (high == low) return high;
-        int mid = (low + high) / 2;
+        int lastIndex = start;
 
-        if (arr.get(mid) == key && arr.get(mid + 1) != key) return mid;
+        if (firstIndex >= lastIndex) {
+            return new int[]{-1, -1};
+        }
 
-        if (arr.get(mid) > key) return findHighIndex(arr, low, mid, key);
-
-        return findHighIndex(arr, mid, high, key);
+        return new int[]{firstIndex, lastIndex - 1};
     }
 
 }
--- src/test/java/com/codingChallenge/FindFirstandLastPositionOfElementInSortedArray/FindFirstAndLastPositionOfElementInSortedArrayTest.java
+++ src/test/java/com/codingChallenge/FindFirstandLastPositionOfElementInSortedArray/FindFirstAndLastPositionOfElementInSortedArrayTest.java
@@ -6,16 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FindFirstAndLastPositionOfElementInSortedArrayTest {
 
-//    @Test
-//    void positiveTest() {
-//        int[] test = {5, 7, 7, 8, 8, 10};
-//        assertArrayEquals(new int[]{3, 4}, FindFirstAndLastPositionOfElementInSortedArray.searchRange(test, 8));
-//    }
-//
-//    @Test
-//    void negative() {
-//        int[] test = {5, 7, 7, 8, 8, 10};
-//        assertNotEquals(new int[]{0, 0}, FindFirstAndLastPositionOfElementInSortedArray.searchRange(test, 16));
-//    }
+    @Test
+    void positiveTest() {
+        assertArrayEquals(new int[]{3, 4}, FindFirstAndLastPositionOfElementInSortedArray.searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8));
+    }
 
+    @Test
+    void negativeTest() {
+        assertNotEquals(new int[]{0,0}, FindFirstAndLastPositionOfElementInSortedArray.searchRange(new int[] {}, 0));
+    }
 }
