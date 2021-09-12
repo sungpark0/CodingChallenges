650b2d4c59e0acd919dd971bf3491c497680cc68
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-09-11 23:37:49 -0400

    Day327 BinaryTreeLEvelOrderTraversal

--- src/main/java/com/codingChallenge/Tree/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/com/codingChallenge/Tree/BinaryTreeLevelOrderTraversal.java
@@ -27,6 +27,30 @@ public class BinaryTreeLevelOrderTraversal extends BinarySearchTree {
         return result;
     }
 
+    public static List<List<Integer>> levelOrderII(TreeNode root) {
+        if (root == null) return List.of();
+
+        List<List<Integer>> orderList = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.remove();
+                list.add(node.val);
+
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+
+            orderList.add(list);
+        }
+
+        return orderList;
+    }
+
     public static void main(String[] args) {
 
     }
--- src/test/java/com/codingChallenge/Tree/BinaryTreeLevelOrderTraversalTest.java
+++ src/test/java/com/codingChallenge/Tree/BinaryTreeLevelOrderTraversalTest.java
@@ -37,4 +37,27 @@ class BinaryTreeLevelOrderTraversalTest {
                 List.of(10),
                 List.of(20)), BinaryTreeLevelOrderTraversal.levelOrder(tree.root));
     }
+
+    @Test
+    void positiveTestII() {
+        tree.insertRecursion(3);
+        tree.insertRecursion(9);
+        tree.insertRecursion(20);
+        tree.insertRecursion(15);
+        tree.insertRecursion(7);
+
+        assertEquals(List.of(
+                List.of(3),
+                List.of(9),
+                List.of(7, 20),
+                List.of(15)), BinaryTreeLevelOrderTraversal.levelOrderII(tree.root));
+    }
+
+    @Test
+    void negativeTestII() {
+        tree.insertRecursion(1);
+        tree.insertRecursion(3);
+
+        assertNotEquals(List.of(List.of(1)), BinaryTreeLevelOrderTraversal.levelOrderII(tree.root));
+    }
 }
