7f3acbb94c5547b6ac00113dc32f3fa6d3d0598e
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-09-26 00:04:51 -0400

    Day341 NumberOfSubstringsContainingAllThreeCharacters

--- src/main/java/com/codingChallenge/NumberOfSubstringsContainingAllThreeCharacters/NumberOfSubstringsContainingAllThreeCharacters.java
+++ src/main/java/com/codingChallenge/NumberOfSubstringsContainingAllThreeCharacters/NumberOfSubstringsContainingAllThreeCharacters.java
@@ -1,6 +1,27 @@
 package com.codingChallenge.NumberOfSubstringsContainingAllThreeCharacters;
 
 public class NumberOfSubstringsContainingAllThreeCharacters {
+    public static int numberOfSubstrings(String s) {
+        int counter = 0;
+        int[] chars = new int[3];
+        int startingIndex = 0;
+        int currentIndex = 0;
+
+        while (currentIndex < s.length()) {
+            while (!(chars[0] > 0 && chars[1] > 0 && chars[2] > 0) && currentIndex < s.length()) {
+                chars[s.charAt(currentIndex) - 'a']++;
+                currentIndex++;
+            }
+
+            while (chars[0] > 0 && chars[1] > 0 && chars[2] > 0) {
+                counter += s.length() - currentIndex + 1;
+                chars[s.charAt(startingIndex) - 'a']--;
+                startingIndex++;
+            }
+        }
+
+        return counter;
+    }
 
     public static void main(String[] args) {
 
--- src/test/java/com/codingChallenge/NumberOfSubstringsContainingAllThreeCharacters/NumberOfSubstringsContainingAllThreeCharactersTest.java
+++ src/test/java/com/codingChallenge/NumberOfSubstringsContainingAllThreeCharacters/NumberOfSubstringsContainingAllThreeCharactersTest.java
@@ -1,7 +1,18 @@
 package com.codingChallenge.NumberOfSubstringsContainingAllThreeCharacters;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberOfSubstringsContainingAllThreeCharactersTest {
 
+    @Test
+    void positiveTest() {
+        assertEquals(10, NumberOfSubstringsContainingAllThreeCharacters.numberOfSubstrings("abcabc"));
+    }
+
+    @Test
+    void negativeTest() {
+        assertNotEquals(2, NumberOfSubstringsContainingAllThreeCharacters.numberOfSubstrings("aaacb"));
+    }
 }
