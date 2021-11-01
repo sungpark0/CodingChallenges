898efbeaf5c7d7c7e1794e963c3a4d53721260df
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-10-31 23:29:39 -0400

    Day377 LargestOddNumberInString

--- src/main/java/com/codingChallenge/MaxAreaOfIsland/MaxAreaOfIsland.java
+++ src/main/java/com/codingChallenge/MaxAreaOfIsland/MaxAreaOfIsland.java
@@ -17,11 +17,18 @@ public class MaxAreaOfIsland {
     }
 
     public static int dfs(int[][] grid, int col, int row) {
-        if (col < 0 || row < 0 || col >= grid.length || row >= grid[0].length || grid[col][row] == 0) return 0;
+        if (col < 0 || row < 0 ||
+                col >= grid.length ||
+                row >= grid[0].length ||
+                grid[col][row] == 0) return 0;
 
         grid[col][row] = 0;
 
-        return 1 + dfs(grid, col + 1, row) + dfs(grid, col - 1, row) + dfs(grid, col, row + 1) + dfs(grid, col, row - 1);
+        return 1 +
+                dfs(grid, col + 1, row) +
+                dfs(grid, col - 1, row) +
+                dfs(grid, col, row + 1) +
+                dfs(grid, col, row - 1);
     }
 
     public static void main(String[] args) {
