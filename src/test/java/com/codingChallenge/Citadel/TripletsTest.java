4398cb0271adb9c9962073b9424ea09a14211903
sungpark0 <72265464+sungpark96@users.noreply.github.com>
2021-11-28 23:52:41 -0500

    Update on triplets

--- src/test/java/com/codingChallenge/Citadel/TripletsTest.java
+++ src/test/java/com/codingChallenge/Citadel/TripletsTest.java
@@ -2,8 +2,7 @@ package com.codingChallenge.Citadel;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
